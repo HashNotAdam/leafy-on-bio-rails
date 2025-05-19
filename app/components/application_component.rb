04af4a63a8f9dd6df46d27ac8672ecdff078816d
Adam Rice <adamr@bioconcepts.com.au>
2025-05-19 15:20:58 +1000

    Create ApplicationComponent

---
+++ app/components/application_component.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class ApplicationComponent < ViewComponent::Base
+  include Stimulizer
+  include Turbo::FramesHelper
+  include Turbo::StreamsHelper
+
+  private
+
+  # Converts a component's class name to a CSS class name
+  # Example: Module::MyComponent => module--my-component
+  def component_css_class
+    @component_css_class ||= self.class.name
+      .gsub(/(.)([A-Z])/, '\1-\2')
+      .gsub("::", "-")
+      .sub("Components--", "")
+      .downcase
+  end
+end
